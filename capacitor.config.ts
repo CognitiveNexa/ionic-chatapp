7807b9f01ce53ce41311c8c9346b1185cd4e0571
Zeshan <zeshanshakil0@gmail.com>
2024-06-21 13:18:15 +0500

    feat: android development

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionCalender',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https',
+  },
+};
+
+export default config;
