1aec28c83264c422726ded2d0ff4fc574dddba3f
Zeshan <zeshanshakil0@gmail.com>
2024-06-21 13:12:41 +0500

    feat: finalization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,27 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    loadChildren: () => import('./page/login/login.module').then((m) => m.LoginPageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'register',
+    loadChildren: () => import('./page/register/register.module').then((m) => m.RegisterPageModule),
   },
+  {
+    path: 'groups/:groupId',
+    loadChildren: () => import('./page/messages/messages.module').then((m) => m.MessagesPageModule),
+  },
+  {
+    path: 'groups',
+    loadChildren: () => import('./page/groups/groups.module').then((m) => m.GroupsPageModule)
+  }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
